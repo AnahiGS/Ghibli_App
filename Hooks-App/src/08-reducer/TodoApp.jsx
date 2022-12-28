d3514879776e2893729bc75d7ce86c979d6ba2eb
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-12-27 20:57:32 -0600

    reducer

---
+++ Hooks-App/src/08-reducer/TodoApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TodoApp = () => {
+  return (
+    <>
+      <h1>TodoApp</h1>
+      <hr/>
+      <ul>
+        <li>Item1</li>
+        <li>Item1</li>
+        <li>Item1</li>
+      </ul>
+    </>
+  )
+}
+
+export default TodoApp
---
+++ Hooks-App/src/08-reducer/intro-reducer.js
@@ -0,0 +1,18 @@
+se usa cuando tenemos un objeto complejo y queremos hacerle algunos cambios
+
+const initialState = [{
+  id:1,
+  todo:'Recolectar la piedra del Alma',
+  done: false,
+}];
+
+const todoReducer = (state=initialState, action) => {
+
+  return state;
+}
+
+let todos = todoReducer();
+
+const newTodo={
+  
+}
