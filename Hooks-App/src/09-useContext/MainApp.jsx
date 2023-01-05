56df5f08027d79e6298656e62898eb3affe67699
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2023-01-04 21:23:04 -0600

    useContext

---
+++ Hooks-App/src/09-useContext/MainApp.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Routes, Route, Navigate} from 'react-router-dom'
+import AboutPage from './AboutPage'
+import { HomePage } from './HomePage'
+import LoginPage from './LoginPage'
+import Navbar from './Navbar'
+
+
+const MainApp = () => {
+  return (
+    <UserProvider>
+      <h1>MainApp</h1>
+      <hr/>
+
+      <Navbar/>
+
+
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='about' element={<AboutPage/>}/>
+        <Route path='login' element={<LoginPage/>}/>
+        <Route path='/*' element={<Navigate to="/about
+        "/>}/>
+      </Routes>
+    </UserProvider>
+  )
+}
+
+export default MainApp
--- Hooks-App/src/main.jsx
+++ Hooks-App/src/main.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import SimpleForm from './02-useEffect/SimpleForm'
 import HooksApp from './HooksApp'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SimpleForm />
-  </React.StrictMode>
-)
+  //high order component, recibe otros componemtes dentro de él
+  <BrowserRouter> 
+  
+    <React.StrictMode>
+      
+      <SimpleForm />
+    </React.StrictMode>
+  </BrowserRouter>
+  )
